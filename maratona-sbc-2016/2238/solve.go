25597c6bf514b27a776342b6d17cc99da9935fea
Jackson Barbosa <jbs@ic.ufal.br>
2022-09-28 00:11:25 -0300

    fixed the go solution to bee 2238

--- maratona-sbc-2016/2238/solve.go
+++ maratona-sbc-2016/2238/solve.go
@@ -5,14 +5,13 @@ import (
 )
 
 func main() {
-    var A, B, C, D, x, ans, i int64
-	var sqrt_c float64
+    var A, B, C, D, x, ans, i, sqrt_c int64
     ans = -1
     fmt.Scanln(&A, &B, &C, &D)
 	i = 0
-	sqrt_c = math.Sqrt(C)
+	sqrt_c = int64(math.Sqrt(float64(C)))
 	for i <= sqrt_c {
-		if (x % B) && !(C % x) && (D % x) {
+		if (x % B) != 0 && (C % x) == 0 && (D % x) != 0 {
 			ans = x
 			break
 		}
